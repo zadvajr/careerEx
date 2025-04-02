02e6a56b3a175bab7d26ea667be97f1c314dcc0b
unknown <danielzadva@gmail.com>
2025-04-02 23:19:25 +0100

    initial setup

---
+++ youthrive_cohort_2/live_classes/week_6/dom_manipulation/scripts/index.js
@@ -0,0 +1,12 @@
+//what is DOM
+//how to select html elements
+//DOM Navigation
+//getting and changing html text
+//changing element attributes
+//adding event listeners
+//classes and styles
+//creating elements
+
+//how to select htmll elements
+//1. document.getElementById()
+//2. document.querySelector()
